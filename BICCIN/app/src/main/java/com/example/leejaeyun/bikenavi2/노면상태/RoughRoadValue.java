3b84318d2d7722b4a746689802f3864bd6256f17
wendy831 <wendy831@naver.com>
2017-06-24 20:35:44 +0900

    노면상태

---
+++ BICCIN/app/src/main/java/com/example/leejaeyun/bikenavi2/노면상태/RoughRoadValue.java
@@ -0,0 +1,31 @@
+package com.example.leejaeyun.bikenavi2.Tmap;
+
+/**
+ * Created by Lee Jae Yun on 2017-06-11.
+ */
+
+public class RoughRoadValue {
+
+    double latitude;
+    double longitude;
+
+    public double getLatitude()
+    {
+        return latitude;
+    }
+
+    public double getLongitude()
+    {
+        return longitude;
+    }
+
+    public void setLatitude(double latitude)
+    {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude)
+    {
+        this.longitude = longitude;
+    }
+}
