7f37b6dd715a112177ae39009ee8d40e63954b19
wendy831 <wendy831@naver.com>
2017-06-24 20:33:33 +0900

    사고지역 update

---
+++ BICCIN/app/src/main/java/com/example/leejaeyun/bikenavi2/사고지역 update/AccidentValue.java
@@ -0,0 +1,27 @@
+package com.example.leejaeyun.bikenavi2.Tmap;
+
+/**
+ * Created by Lee Jae Yun on 2017-06-01.
+ */
+
+public class AccidentValue {
+
+    double latitude;
+    double longitude;
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
